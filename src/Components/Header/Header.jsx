74cba8e08e0c55782c94357859c236635d9b9ba9
Sieugene <37626545+sieugene@users.noreply.github.com>
2020-01-27 21:26:38 +0300

    upload photos for github

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = (props) => {
                         </button>
                     </NavLink>
                 }
-                dfgdff
+
             </div>
         </div>
     )
